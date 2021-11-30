2bd36aa50911828ad44f502af0d8ee894e1221b6
Floozutter <me@floozutter.site>
2021-11-29 20:27:28 -0800

    remove redundant faux_cmd.py

--- trashifier/prod/faux_cmd.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-print("I figured out how to interact with the Terminal using python!")
-print("(Triple colon implies terminal call.)")
-print("- - - - -\n")
-
-while True:
-    subprocess.Popen(input("::: "), shell=True).wait()
