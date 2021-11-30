22b034c6a8fea6ce585ebe3587052fa799b59ce9
Floozutter <me@floozutter.site>
2021-11-29 20:25:01 -0800

    clean up trashifier.py and trashifier2.py
    trashifier.py is for training and validation
    trashifier2.py is for testing

--- trashifier/train/trashifier.py
+++ trashifier/train/trashifier.py
@@ -80,10 +80,6 @@ valid_data_loader = DataLoader(data['valid'], batch_size=bs, shuffle=True)
 test_data_loader = DataLoader(data['test'], batch_size=bs, shuffle=True)
 
 
-train_data_size, valid_data_size, test_data_size
-
-
-
 # Load pretrained ResNet50 Model
 resnet50 = models.resnet50(pretrained=True)
 #resnet50 = resnet50.to('cuda:0')
@@ -252,8 +248,6 @@ plt.ylim(0,1)
 plt.savefig(dataset+'_loss_curve.png')
 plt.show()
 
-
-
 plt.plot(history[:,2:4])
 plt.legend(['Tr Accuracy', 'Val Accuracy'])
 plt.xlabel('Epoch Number')
@@ -261,129 +255,3 @@ plt.ylabel('Accuracy')
 plt.ylim(0,1)
 plt.savefig(dataset+'_accuracy_curve.png')
 plt.show()
-
-def computeTestSetAccuracy(model, loss_criterion):
-    '''
-    Function to compute the accuracy on the test set
-    Parameters
-        :param model: Model to test
-        :param loss_criterion: Loss Criterion to minimize
-    '''
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    test_acc = 0.0
-    test_loss = 0.0
-
-    # Validation - No gradient tracking needed
-    with torch.no_grad():
-
-        # Set to evaluation mode
-        model.eval()
-
-        # Validation loop
-        for j, (inputs, labels) in enumerate(test_data_loader):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            # Forward pass - compute outputs on input data using the model
-            outputs = model(inputs)
-
-            # Compute loss
-            loss = loss_criterion(outputs, labels)
-
-            # Compute the total loss for the batch and add it to valid_loss
-            test_loss += loss.item() * inputs.size(0)
-
-            # Calculate validation accuracy
-            ret, predictions = torch.max(outputs.data, 1)
-            correct_counts = predictions.eq(labels.data.view_as(predictions))
-
-            # Convert correct_counts to float and then compute the mean
-            acc = torch.mean(correct_counts.type(torch.FloatTensor))
-
-            # Compute total accuracy in the whole batch and add to valid_acc
-            test_acc += acc.item() * inputs.size(0)
-
-            print("Test Batch number: {:03d}, Test: Loss: {:.4f}, Accuracy: {:.4f}".format(j, loss.item(), acc.item()))
-
-    # Find average test loss and test accuracy
-    avg_test_loss = test_loss/test_data_size 
-    avg_test_acc = test_acc/test_data_size
-
-    print("Test accuracy : " + str(avg_test_acc))
-
-
-
-def predict(model, test_image_name):
-    '''
-    Function to predict the class of a single test image
-    Parameters
-        :param model: Model to test
-        :param test_image_name: Test image
-
-    '''
-    
-    transform = image_transforms['test']
-
-    test_image = Image.open(test_image_name)
-    plt.imshow(test_image)
-    
-    test_image_tensor = transform(test_image)
-
-    if torch.cuda.is_available():
-        test_image_tensor = test_image_tensor.view(1, 3, 224, 224).cuda()
-    else:
-        test_image_tensor = test_image_tensor.view(1, 3, 224, 224)
-    
-    with torch.no_grad():
-        model.eval()
-        # Model outputs log probabilities
-        out = model(test_image_tensor)
-        ps = torch.exp(out)
-        topk, topclass = ps.topk(3, dim=1)
-        for i in range(3):
-            print("Predcition", i+1, ":", idx_to_class[topclass.cpu().numpy()[0][i]], ", Score: ", topk.cpu().numpy()[0][i])
-
-
-
-
-def predict(model, test_image_name):
-    '''
-    Function to predict the class of a single test image
-    Parameters
-        :param model: Model to test
-        :param test_image_name: Test image
-
-    '''
-    
-    transform = image_transforms['test']
-
-    test_image = Image.open(test_image_name)
-    plt.imshow(test_image)
-    
-    test_image_tensor = transform(test_image)
-
-    if torch.cuda.is_available():
-        test_image_tensor = test_image_tensor.view(1, 3, 224, 224).cuda()
-    else:
-        test_image_tensor = test_image_tensor.view(1, 3, 224, 224)
-    
-    with torch.no_grad():
-        model.eval()
-        # Model outputs log probabilities
-        out = model(test_image_tensor)
-        ps = torch.exp(out)
-        topk, topclass = ps.topk(3, dim=1)
-        for i in range(3):
-            print("Predcition", i+1, ":", idx_to_class[topclass.cpu().numpy()[0][i]], ", Score: ", topk.cpu().numpy()[0][i])
-
-
-# Test a particular model on a test image
-
-dataset = 'caltech_10'
-model = torch.load('caltech_10_model_8.pt')
-predict(model, 'pixabay-test-animals/triceratops-954293_640.jpg')
-
-# Load Data from folders
-#computeTestSetAccuracy(model, loss_func)
